de5e36d2cbef4febab1a3efbb85805d117606558
HexagonHQI <163999976+HexagonHQI@users.noreply.github.com>
2025-01-11 00:32:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap
 
 ReactDOM.render(
     <React.StrictMode>
